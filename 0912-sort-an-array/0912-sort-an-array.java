a7d82eb1fff747934cee19f2e02908fa7e8608d6
Naman Kankaria <naman.23bcon1072@jecrcu.edu.in>
2026-06-23 02:02:32 +0530

    Time: 283 ms (8.51%), Space: 83.4 MB (47.49%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,32 @@
+class Solution {
+      public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+     public static void quicksort(int[] arr, int low, int high){
+        if(low<high){
+            int pivot = partition(arr,low,high);
+
+            quicksort(arr,low,pivot-1);
+            quicksort(arr,pivot+1,high);
+        }
+
+    }
+    public static int partition(int[] arr, int low, int high){
+        int pivot = arr[high];
+        int i = low-1;
+
+        for(int j= low; j<high; j++){
+            if(arr[j]<pivot){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        return i+1;
+    }
+}
