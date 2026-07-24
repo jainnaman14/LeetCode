ca57a92f050233eb7455a99f1908a6bd26756b0c
Naman Kankaria <naman.23bcon1072@jecrcu.edu.in>
2026-07-24 20:44:29 +0530

    Time: 26 ms (61.95%), Space: 86.7 MB (18.29%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,32 +1,42 @@
 class Solution {
-      public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
         return nums;
     }
-     public static void quicksort(int[] arr, int low, int high){
-        if(low<high){
-            int pivot = partition(arr,low,high);
 
-            quicksort(arr,low,pivot-1);
-            quicksort(arr,pivot+1,high);
-        }
+    private void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) return;
 
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
-    public static int partition(int[] arr, int low, int high){
-        int pivot = arr[high];
-        int i = low-1;
 
-        for(int j= low; j<high; j++){
-            if(arr[j]<pivot){
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+    private void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
             }
         }
-        int temp = arr[i+1];
-        arr[i+1] = arr[high];
-        arr[high] = temp;
-        return i+1;
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        for (i = 0; i < temp.length; i++) {
+            arr[left + i] = temp[i];
+        }
     }
 }
