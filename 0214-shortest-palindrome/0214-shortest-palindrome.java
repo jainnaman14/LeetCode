daeac330b05ee2f12a6df11d57d3f9d550888269
Naman Kankaria <naman.23bcon1072@jecrcu.edu.in>
2026-08-03 20:03:53 +0530

    Time: 8 ms (59.89%), Space: 46.3 MB (93.71%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+        if (s == null || s.length() <= 1) {
+            return s;
+        }
+
+        String reversed = new StringBuilder(s).reverse().toString();
+
+        String combined = s + "#" + reversed;
+
+        int[] lps = new int[combined.length()];
+
+        for (int i = 1; i < combined.length(); i++) {
+            int j = lps[i - 1];
+
+            while (j > 0 && combined.charAt(i) != combined.charAt(j)) {
+                j = lps[j - 1];
+            }
+
+            if (combined.charAt(i) == combined.charAt(j)) {
+                j++;
+            }
+
+            lps[i] = j;
+        }
+
+        int palindromeLength = lps[combined.length() - 1];
+
+        String remaining = s.substring(palindromeLength);
+
+        return new StringBuilder(remaining).reverse().toString() + s;
+    }
+}
